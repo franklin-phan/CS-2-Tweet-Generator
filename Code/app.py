2fd1863d35819013c2a9eebc1c1a0d32f2d77dd2
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-03-05 00:00:24 -0800

    trying to heroku

--- Code/app.py
+++ Code/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return histogram('despacito.txt')
+    return ("Hello World")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+
